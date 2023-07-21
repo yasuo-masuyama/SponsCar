5bef9021832ceae3340da3d924f8c1ac62bc5bd1
Yasuo Masuyama <y.m19980526@gmail.com>
2023-07-21 13:30:13 +0900

    :sparkles: seedデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  id: 1,
+  email: 'admin1@gmail.com',
+  name: "マスター", name_kana:"マスター",
+  password: "admin1",
+  password_confirmation: "admin1",
+  )
