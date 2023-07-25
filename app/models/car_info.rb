56efa42dd469c5a7eb8f835d1092b8e36f1257be
Yasuo Masuyama <y.m19980526@gmail.com>
2023-07-25 14:47:34 +0900

    :sparkles: 画像投稿機能の追加

--- app/models/car_info.rb
+++ app/models/car_info.rb
@@ -1,3 +1,6 @@
 class CarInfo < ApplicationRecord
+  validates :vehicle_image, presence: true, blob: { content_type: :image }
+
   belongs_to :driver
+  has_one_attached :vehicle_image
 end
--- app/models/driver.rb
+++ app/models/driver.rb
@@ -4,9 +4,20 @@ class Driver < ApplicationRecord
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   
+  validates_acceptance_of :terms, allow_nil: false, message: "※会員登録には利用規約への同意が必要です。", on: :create
+  validates :name, presence: true
+  validates :name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email, presence: true
+
+  has_one_attached :profile_image
+  has_one_attached :license_image
+  
   has_many :under_deals
   has_many :car_infos
   has_many :transfer_infos
-
+  
   enum user_status: {not_check: 0, checked_indentification: 1, checked_car_indentification: 2,}
 end
