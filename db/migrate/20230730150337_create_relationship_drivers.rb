9c121244b7a533790cb8adebb4ed23b6e2304c4e
Yasuo Masuyama <y.m19980526@gmail.com>
2023-07-31 00:10:52 +0900

    :+1: カラムの追加

--- db/migrate/20230730150337_create_relationship_drivers.rb
+++ db/migrate/20230730150337_create_relationship_drivers.rb
@@ -1,8 +1,13 @@
 class CreateRelationshipDrivers < ActiveRecord::Migration[6.1]
   def change
     create_table :relationship_drivers do |t|
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
 
       t.timestamps
     end
+    add_index :relationship_drivers, :follower_id
+    add_index :relationship_drivers, :followed_id
+    add_index :relationship_drivers, [:follower_id, :followed_id], unique: true
   end
 end
