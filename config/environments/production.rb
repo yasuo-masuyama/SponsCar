670d236eb21d4c2534200ed2c2525782841ca4ae
Yasuo Masuyama <y.m19980526@gmail.com>
2023-08-16 14:50:05 +0900

    :bug: バグ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,5 +117,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.host_authorization = { exclude: ->(request) { request.path == '/health_checks' } }
 end
